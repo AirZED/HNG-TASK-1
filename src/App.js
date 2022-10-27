fbc92552547186fca0de542f1e80bc0c6ee8176c
Mfoniso <ukpabiomfoniso@gmail.com>
2022-10-27 16:25:20 +0100

    Finished Responsiveness and entire UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import { Fragment } from "react";
+
 //importing components
 import TreeLink from "./components/TreeLink";
 import Profile from "./components/Profile";
+import MediaIcons from "./components/MediaIcon";
 
 //importing styles
 import classes from "./App.module.css";
+import Footer from "./components/Footer";
 
 function App() {
+  //links shortened
+  const links = (
+    <Fragment>
+      <TreeLink href="https://twitter.com/Mfonisoh1">Twitter Link</TreeLink>
+      <TreeLink href="https://training.zuri.team/">Zuri Team</TreeLink>
+      <TreeLink href="http://books.zuri.team">Zuri Books</TreeLink>
+      <TreeLink href="https://books.zuri.team/python-for-beginners?ref_id=mfoniso">
+        Python Books
+      </TreeLink>
+      <TreeLink href="https://background.zuri.team">
+        Background Check for Coders
+      </TreeLink>
+      <TreeLink href="https://books.zuri.team/design-rules">
+        Design Books
+      </TreeLink>
+      <MediaIcons />
+    </Fragment>
+  );
   return (
     <div className={classes.App}>
       <Profile />
-      <main>
-        <TreeLink href="https://training.zuri.team/">Twitter Link</TreeLink>
-        <TreeLink href="twitter.com/.">Zuri Team</TreeLink>
-        <TreeLink href="twitter.com/.">Zuri Books</TreeLink>
-        <TreeLink href="twitter.com/.">Python Books</TreeLink>
-        <TreeLink href="twitter.com/.">Background Ckeck for Coders</TreeLink>
-        <TreeLink href="twitter.com/.">Design Books</TreeLink>
-      </main>
+      <main>{links}</main>
+      <Footer />
     </div>
   );
 }
